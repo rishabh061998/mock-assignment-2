8f6c8e82d15f03a52a2a2e1f195911ab171b51ba
rishabh061998 <rishabhjune06.1998@gmail.com>
2022-10-15 12:34:27 +0530

    commit-1

---
+++ script.js
@@ -0,0 +1,22 @@
+fetch("https://dog.ceo/api/breed/hound/images").then((data)=>{
+    return data.json();
+    // console.log(data)
+}).then((completedata)=>{
+    // console.log(completedata.message);
+    // document.getElementById("root").innerHTML=completedata.message;
+   let data1="";
+   completedata.message.map((values)=>{
+    data1+=` <div class="card">
+    <h1 class="title">title</h1>
+    <img src=${values} alt="img" class="images">
+    <p>Description</p>
+    <p class="category">Category</p>
+</div>`
+   });
+
+   document.getElementById("cards").innerHTML=data1;
+  
+
+}).catch((err)=>{
+    console.log(err)
+})
